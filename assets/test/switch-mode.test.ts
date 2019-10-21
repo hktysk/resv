fad9a68c593d9dfd432fe2889e6bfbfb1a0a348c
hkt <komikansougou@gmail.com>
2019-10-21 10:45:53 +0900

    test: added test to switch between pc and mobile

---
+++ assets/test/switch-mode.test.ts
@@ -0,0 +1,44 @@
+import * as fs from 'fs'
+import * as path from 'path'
+import { switchDeviceMode } from '../js/src/switch-mode'
+import 'jest-localstorage-mock'
+
+const html: string = fs.readFileSync(path.resolve(__dirname, '../index.html')).toString()
+
+jest.dontMock('fs')
+
+describe('switching between pc and mobile', () => {
+  let wrap: HTMLElement
+  let btn: {
+    mobile: HTMLElement
+    pc: HTMLElement
+  }
+  beforeEach(() => {
+    document.documentElement.innerHTML = html
+    wrap = document.getElementById('wrap')
+    btn = {
+      mobile: document.getElementById('mobile-mode'),
+      pc: document.getElementById('pc-mode')
+    }
+
+    switchDeviceMode()
+  })
+  afterEach(() => {
+    jest.resetModules()
+  })
+
+  it('when clicked mobile-mode', () => {
+    btn.pc.click()
+    btn.mobile.click()
+    expect(wrap.classList.contains('pc-mode')).toBe(false)
+    expect(localStorage.getItem('mode')).toBe('mobile')
+  })
+
+  it('when clicked pc-mode', () => {
+    btn.mobile.click()
+    btn.pc.click()
+    expect(wrap.classList.contains('pc-mode')).toBe(true)
+    expect(localStorage.getItem('mode')).toBe('pc')  })
+
+})
+
