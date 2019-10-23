7580d02e201f5deadf56646830fd0f0c790e84f3
hkt <komikansougou@gmail.com>
2019-10-23 11:51:42 +0900

    style: add line break to rotate.js

--- assets/js/src/rotate.ts
+++ assets/js/src/rotate.ts
@@ -1,6 +1,7 @@
 export function rotate(): void {
   const btn: HTMLElement = document.getElementById('rotate-icon')
   const wrap: HTMLElement = document.getElementById('wrap')
+
   btn.onclick = () => {
     if (btn.classList.contains('rotate-icon-stop')) return
 
